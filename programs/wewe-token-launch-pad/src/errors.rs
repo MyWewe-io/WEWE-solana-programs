d75f595ec3a3d10e6bf4ecbcfe1492cc5eb4a1c0
Pablitosinyores <nikhilbajajj01@gmail.com>
2025-03-11 18:00:51 +0530

    feat: add errors and constants

---
+++ programs/wewe-token-launch-pad/src/errors.rs
@@ -0,0 +1,21 @@
+use anchor_lang::error_code;
+
+#[error_code]
+pub enum ProposalError {
+    #[msg("The amount to raise has not been met")]
+    TargetNotMet,
+    #[msg("The amount to raise has been achieved")]
+    TargetMet,
+    #[msg("The contribution is too big")]
+    ContributionTooBig,
+    #[msg("The contribution is too small")]
+    ContributionTooSmall,
+    #[msg("The maximum amount to contribute has been reached")]
+    MaximumContributionsReached,
+    #[msg("The backing has not ended yet")]
+    BackingNotEnded,
+    #[msg("The backing has ended")]
+    BackingEnded,
+    #[msg("Invalid total amount. i should be bigger than 3")]
+    InvalidAmount
+}
--- programs/wewe-token-launch-pad/src/lib.rs
+++ programs/wewe-token-launch-pad/src/lib.rs
@@ -1,18 +1,4 @@
 pub mod state;
 pub mod instructions;
-use anchor_lang::prelude::*;
-
-declare_id!("DtwC3LsBgwnp6Cuc6MExnijmwh7WLXS5Hdr7XpdyF1qZ");
-
-#[program]
-pub mod wewe_token_launch_pad {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
-}
-
-#[derive(Accounts)]
-pub struct Initialize {}
+pub mod errors;
+mod constant;
