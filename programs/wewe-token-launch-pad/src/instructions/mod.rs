e7dd511271672abf61ec79011739720eb28cd9b9
Pablitosinyores <nikhilbajajj01@gmail.com>
2025-03-11 18:01:28 +0530

    feat: add instructions

---
+++ programs/wewe-token-launch-pad/src/instructions/mod.rs
@@ -0,0 +1,3 @@
+pub mod backtoken;
+
+pub use backtoken;
--- programs/wewe-token-launch-pad/src/state/proposer.rs
+++ programs/wewe-token-launch-pad/src/state/proposer.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(InitSpace)]
 pub struct Proposer {
-    pub maker: Pubkey,
+    pub proposer: Pubkey,
     pub time_started: i64,
     pub duration: u16,
     pub protocol: str,
