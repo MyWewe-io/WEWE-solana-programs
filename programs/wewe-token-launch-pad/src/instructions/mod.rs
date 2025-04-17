946ac5810968b91898f46a5f603568e16016afd1
Pablitosinyores <nikhilbajajj01@gmail.com>
2025-04-17 20:49:55 +0530

    feat: token creation in create proposal

--- programs/wewe-token-launch-pad/src/instructions/mod.rs
+++ programs/wewe-token-launch-pad/src/instructions/mod.rs
@@ -1,13 +1,11 @@
 pub mod back_token;
 pub mod create_pool;
-pub mod create_token;
 pub mod proposal;
 pub mod refund;
 pub mod distribute_token;
 
 pub use back_token::*;
 pub use create_pool::*;
-pub use create_token::*;
 pub use proposal::*;
 pub use refund::*;
 pub use distribute_token::*;
--- programs/wewe-token-launch-pad/src/instructions/proposal.rs
+++ programs/wewe-token-launch-pad/src/instructions/proposal.rs
@@ -1,11 +1,20 @@
 use anchor_lang::prelude::*;
 
-use crate::{
-    constant::ANCHOR_DISCRIMINATOR, state::proposer::Proposal,
-    event::ProposalCreated
+use crate::{event::ProposalCreated, state::proposer::Proposal};
+use {
+    crate::constant::{ANCHOR_DISCRIMINATOR, TOTAL_MINT},
+    anchor_spl::{
+        associated_token::AssociatedToken,
+        metadata::{
+            create_metadata_accounts_v3, mpl_token_metadata::types::DataV2,
+            CreateMetadataAccountsV3, Metadata,
+        },
+        token::{mint_to, Mint, MintTo, Token, TokenAccount},
+    },
 };
 
 #[derive(Accounts)]
+#[instruction(_token_decimals: u8)]
 pub struct CreateProposal<'info> {
     #[account(mut)]
     pub maker: Signer<'info>, // Creator of the proposal
@@ -17,12 +26,92 @@ pub struct CreateProposal<'info> {
         space = ANCHOR_DISCRIMINATOR + Proposal::INIT_SPACE,
     )]
     pub proposal: Account<'info, Proposal>,
+
+    #[account(
+        init,
+        payer = maker,
+        mint::decimals = _token_decimals,
+        mint::authority = maker.key(),
+        mint::freeze_authority = maker.key(),
+
+    )]
+    pub mint_account: Account<'info, Mint>,
+
+    /// CHECK: Validate address by deriving pda
+    #[account(
+        mut,
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), mint_account.key().as_ref()],
+        bump,
+        seeds::program = token_metadata_program.key(),
+    )]
+    pub metadata_account: UncheckedAccount<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = maker,
+        associated_token::mint = mint_account,
+        associated_token::authority = maker,
+    )]
+    pub token_vault: Account<'info, TokenAccount>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Program<'info, Token>,
+    pub token_metadata_program: Program<'info, Metadata>,
     pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
 }
 
 impl<'info> CreateProposal<'info> {
-    pub fn create_proposal(&mut self, duration: u16, backing_goal: u64, bumps: &CreateProposalBumps) -> Result<()> {
-    
+    pub fn create_proposal(
+        &mut self,
+        duration: u16,
+        backing_goal: u64,
+        bumps: &CreateProposalBumps,
+        token_name: String,
+        token_symbol: String,
+        token_uri: String,
+        _token_decimals: u8,
+    ) -> Result<()> {
+        create_metadata_accounts_v3(
+            CpiContext::new(
+                self.token_metadata_program.to_account_info(),
+                CreateMetadataAccountsV3 {
+                    metadata: self.metadata_account.to_account_info(),
+                    mint: self.mint_account.to_account_info(),
+                    mint_authority: self.maker.to_account_info(),
+                    update_authority: self.maker.to_account_info(),
+                    payer: self.maker.to_account_info(),
+                    system_program: self.system_program.to_account_info(),
+                    rent: self.rent.to_account_info(),
+                },
+            ),
+            DataV2 {
+                name: token_name,
+                symbol: token_symbol,
+                uri: token_uri,
+                seller_fee_basis_points: 0,
+                creators: None,
+                collection: None,
+                uses: None,
+            },
+            false, // Is mutable
+            true,  // Update authority is signer
+            None,  // Collection details
+        )?;
+
+        // Invoke the mint_to instruction on the token program
+        mint_to(
+            CpiContext::new(
+                self.token_program.to_account_info(),
+                MintTo {
+                    mint: self.mint_account.to_account_info(),
+                    to: self.token_vault.to_account_info(),
+                    authority: self.maker.to_account_info(),
+                },
+            ),
+            TOTAL_MINT * 10u64.pow(self.mint_account.decimals as u32), // Mint tokens
+        )?;
+
         self.proposal.set_inner(Proposal {
             maker: self.maker.key(),
             current_amount: 0,
@@ -38,7 +127,7 @@ impl<'info> CreateProposal<'info> {
             start_time: Clock::get()?.unix_timestamp,
             duration,
         });
-        
+
         Ok(())
     }
 }
