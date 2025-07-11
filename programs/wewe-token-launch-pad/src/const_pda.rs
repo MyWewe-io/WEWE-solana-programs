5d1e620ec60e0eac92aa6171560b9478c8761bba
Nik <151941421+Pablitosinyores@users.noreply.github.com>
2025-07-11 21:05:56 +0530

    feat: update const pdas and constants

--- programs/wewe-token-launch-pad/src/const_pda.rs
+++ programs/wewe-token-launch-pad/src/const_pda.rs
@@ -1,7 +1,7 @@
 use anchor_lang::solana_program::pubkey::Pubkey;
 use const_crypto::ed25519;
 
-pub mod pool_authority {
+pub mod const_authority {
     use super::*;
 
     const POOL_AUTHORITY_AND_BUMP: ([u8; 32], u8) = ed25519::derive_program_address(
@@ -17,4 +17,11 @@ pub mod pool_authority {
     );
     
     pub const VAULT_BUMP: u8 = VAULT_AUTHORITY_AND_BUMP.1;
+    
+    const MINT_ACCOUNT_AND_BUMP: ([u8; 32], u8) = ed25519::derive_program_address(
+        &[crate::constant::MINT_ACCOUNT],
+        &crate::ID_CONST.to_bytes(),
+    );
+
+    pub const MINT: Pubkey = Pubkey::new_from_array(MINT_ACCOUNT_AND_BUMP.0);
 }
