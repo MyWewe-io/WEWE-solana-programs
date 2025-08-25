82cfc662595b8ee6708cff37a43513154273ac81
Nik <151941421+Pablitosinyores@users.noreply.github.com>
2025-08-25 14:35:41 +0530

    feat: update ix-airdrop

---
+++ programs/wewe-token-launch-pad/src/instructions/ix_airdrop.rs
@@ -0,0 +1,103 @@
+use crate::{
+    const_pda::const_authority::VAULT_BUMP,
+    constant::{
+        seeds::{BACKER, TOKEN_VAULT, VAULT_AUTHORITY},
+        TOTAL_AIRDROP_AMOUNT_PER_MILESTONE,
+    },
+    errors::ProposalError,
+    event::AirdropClaimed,
+    state::{backers::Backers, proposal::Proposal},
+};
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount, Transfer},
+};
+
+#[derive(Accounts)]
+pub struct Airdrop<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    /// CHECK: backer
+    pub backer: AccountInfo<'info>,
+    pub proposal: Account<'info, Proposal>,
+
+    /// CHECK: vault authority
+    #[account(
+        mut,
+        seeds = [
+            VAULT_AUTHORITY.as_ref(),
+        ],
+        bump,
+    )]
+    pub vault_authority: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    pub mint_account: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        seeds = [TOKEN_VAULT, vault_authority.key().as_ref(), mint_account.key().as_ref()],
+        token::mint = mint_account,
+        token::authority = vault_authority,
+        bump,
+    )]
+    pub token_vault: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds = [BACKER, proposal.key().as_ref(), backer.key().as_ref()],
+        bump,
+    )]
+    pub backer_account: Account<'info, Backers>,
+
+    #[account(
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = mint_account,
+        associated_token::authority = backer,
+    )]
+    pub backer_token_account: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Airdrop<'info> {
+    pub fn airdrop(&mut self) -> Result<()> {
+        require!(self.proposal.is_pool_launched, ProposalError::TargetNotMet);
+
+        let signer_seeds: &[&[&[u8]]] = &[&[VAULT_AUTHORITY, &[VAULT_BUMP]]];
+
+        let amount = TOTAL_AIRDROP_AMOUNT_PER_MILESTONE
+            .checked_div(self.proposal.total_backers)
+            .ok_or(ProposalError::MathOverflow)?;
+
+        anchor_spl::token::transfer(
+            CpiContext::new_with_signer(
+                self.token_program.to_account_info(),
+                Transfer {
+                    from: self.token_vault.to_account_info(),
+                    to: self.backer_token_account.to_account_info(),
+                    authority: self.vault_authority.to_account_info(),
+                },
+                signer_seeds,
+            ),
+            amount * 10u64.pow(9 as u32),
+        )?;
+
+        emit!(AirdropClaimed {
+            proposal_address: self.proposal.key(),
+            backer: self.backer.key(),
+            backer_account: self.backer_account.key(),
+            mint_account: self.mint_account.key(),
+            vault_account: self.token_vault.key(),
+            recipient_account: self.backer_token_account.key(),
+            amount,
+        });
+
+        Ok(())
+    }
+}
--- programs/wewe-token-launch-pad/src/instructions/mod.rs
+++ programs/wewe-token-launch-pad/src/instructions/mod.rs
@@ -3,6 +3,7 @@ pub mod ix_launch_pool;
 pub mod ix_create_proposal;
 pub mod ix_refund;
 pub mod ix_claim_tokens;
+pub mod ix_airdrop;
 pub mod admin;
 
 pub use admin::*;
@@ -11,3 +12,4 @@ pub use ix_launch_pool::*;
 pub use ix_create_proposal::*;
 pub use ix_refund::*;
 pub use ix_claim_tokens::*;
+pub use ix_airdrop::*;
