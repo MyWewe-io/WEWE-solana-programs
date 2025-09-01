4392fd757f528edc0556e423813b7ca35d3e52f5
Nik <151941421+Pablitosinyores@users.noreply.github.com>
2025-09-01 20:21:08 +0530

    feat: update stae

--- programs/wewe-token-launch-pad/src/state/backers.rs
+++ programs/wewe-token-launch-pad/src/state/backers.rs
@@ -6,4 +6,6 @@ pub struct Backers {
     pub claim_amount: u64,
     pub amount_updated_upto_cycle: u8,
     pub initial_airdrop_received: bool,
+    pub contributed_lamports: u64,
+    pub settle_cycle: u8,
 }
